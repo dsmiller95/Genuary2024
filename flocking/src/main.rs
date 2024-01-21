a888028cf56f7519a858a70bd92b7780e21ef0fd
Dan Miller <dsmiller95@gmail.com>
2024-01-21 13:14:35 -0600

    size and center the window on startup

--- flocking/src/main.rs
+++ flocking/src/main.rs
@@ -6,5 +6,13 @@ use boids_sim::app::*;
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, BoidsSimPlugin))
+        .add_systems(Startup, size_window)
         .run();
+}
+
+fn size_window(mut query: Query<&mut Window>) {
+    let mut window = query.single_mut();
+    window.resolution.set_physical_resolution(1920, 1080);
+    window.position.center(MonitorSelection::Current);
+    window.title = "Boids".to_string();
 }
