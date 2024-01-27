603297545723b55cb440f6baafee73f8dc071c3a
Dan Miller <dsmiller95@gmail.com>
2024-01-27 08:55:21 -0600

    use a slightly different transform-apply approach

--- p26-grow-a-seed/src/seed_sim/organ_parent_transform_propigation.rs
+++ p26-grow-a-seed/src/seed_sim/organ_parent_transform_propigation.rs
@@ -3,25 +3,31 @@ use bevy::prelude::Transform;
 use super::prelude::*;
 
 pub fn propagate_custom_transforms(
-    mut query: Query<(Entity, &mut Transform, &OrganRelations, &EntityOrgan)>,
+    mut query: Query<(Entity, &mut Transform, &OrganRelations, &EntityOrgan)>
 )
 {
-    let parent_transforms: HashMap<Entity, Transform> = query.iter()
-        .map(|(entity, transform, _, _)| {
-            (entity, transform.clone())
-        }).collect();
+    let mut changed_transforms : HashMap<Entity, Transform> = HashMap::new();
 
-    for (_e, mut transform, organ_relations, entity_organ) in query.iter_mut() {
+    for (_e, _transform, organ_relations, entity_organ) in query.iter() {
         let parent = match organ_relations.parent {
             Some(parent) => parent,
             None => continue,
         };
-        let parent_transform = match parent_transforms.get(&parent) {
-            Some(transform) => transform,
-            None => continue,
+
+        let parent_transform = match query.get(parent) {
+            Ok((_, trans, _, _)) => trans,
+            Err(_) => continue,
         };
 
         let organ_transform = entity_organ.organ.get_transformation();
-        *transform = parent_transform.mul_transform(organ_transform);
+        changed_transforms.insert(_e, parent_transform.mul_transform(organ_transform));
+    }
+
+    for (entity, mut transform, _, _) in query.iter_mut() {
+        let new_transform = match changed_transforms.get(&entity) {
+            Some(new_transform) => new_transform,
+            None => continue,
+        };
+        *transform = new_transform.clone();
     }
 }
